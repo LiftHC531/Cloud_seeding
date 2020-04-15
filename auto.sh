60f3e859d1d8239f827ed4c493e60c436ad3f00e
artirain <artirain@master.(none)>
2020-04-15 22:16:49 +0800

    update

--- auto.sh
+++ auto.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
-
-cp /work3/artirain/Cloud_seeding/*.ncl .
-cp /work3/artirain/Cloud_seeding/*.m .
-sleep 3 ; chmod +x *.m
-ncl plt_rain.ncl &
-PID=$! 
-ncl cal_cloud.ncl > info1.txt &
-ncl cal_CTH.ncl > info2.txt &
-for job in `jobs -p`
-do
-    echo "Wait job: ${job}"
-    #wait $job
-done
-wait $PID
-matlab -nodesktop -nosplash -nojvm -r "run ./rain_fig.m;quit;"&
+if [ 1 ]
+then
+   cp /work3/artirain/Cloud_seeding/*.ncl .
+   cp /work3/artirain/Cloud_seeding/*.m .
+   sleep 3 ; chmod +x *.m
+   ncl plt_rain.ncl &
+   PID=$! 
+   ncl cal_cloud.ncl >& info1.txt 
+   ncl cal_CTH.ncl >& info2.txt 
+   for job in `jobs -p`
+   do
+       echo "Wait job: ${job}"
+       #wait $job
+   done
+   wait $PID
+   matlab -nodesktop -nosplash -nojvm -r "run ./rain_fig.m;quit;"&
+fi
 
 wait #cal_cloud.ncl & cal_CTH.ncl
-ncl assess.ncl > info3.txt & ; wait
+ncl assess.ncl >& info3.txt ; wait
 echo "assess has done!!"
 ncl plt_score_series.ncl ; wait
 
 FileName=ser_fig.m
 #cp /work3/artirain/Cloud_seeding/$FileName .
 matlab -nodesktop -nosplash -nojvm -r "run ./$FileName;quit;"&
-
+wait
+echo "All Done!!!!! get score_series_panel.png"
