bc1ed475e8e47374422211d11bcbe36f6496bf0c
artirain <artirain@master.(none)>
2020-04-15 10:48:01 +0800

    add accumulated rainfall

--- auto.sh
+++ auto.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 cp /work3/artirain/Cloud_seeding/*.ncl .
-sleep 3
+cp /work3/artirain/Cloud_seeding/*.m .
+sleep 3 ; chmod +x *.m
+ncl plt_rain.ncl &
+PID=$! 
 ncl cal_cloud.ncl > info1.txt &
 ncl cal_CTH.ncl > info2.txt &
 for job in `jobs -p`
@@ -9,14 +12,15 @@ do
     echo "Wait job: ${job}"
     #wait $job
 done
-wait
-ncl assess.ncl > info3.txt &
-PID=$! ; wait $PID
+wait $PID
+matlab -nodesktop -nosplash -nojvm -r "run ./rain_fig.m;quit;"&
+
+wait #cal_cloud.ncl & cal_CTH.ncl
+ncl assess.ncl > info3.txt & ; wait
 echo "assess has done!!"
 ncl plt_score_series.ncl ; wait
 
 FileName=ser_fig.m
-cp /work3/artirain/Cloud_seeding/$FileName .
-sleep 1 ; chmod +x $FileName 
+#cp /work3/artirain/Cloud_seeding/$FileName .
 matlab -nodesktop -nosplash -nojvm -r "run ./$FileName;quit;"&
 
